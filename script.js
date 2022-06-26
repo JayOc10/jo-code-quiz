0c2f72fc3d9cd170ba7884919a89387bb184c6c4
Jeannette Torres <jeannette.ocegueda@yahoo.com>
2022-06-25 18:38:00 -0700

    started with js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Start of assignment
+// Variables
+let quizPage = document.getElementById('quiz-page');
+let timerEl = document.getElementById('timer');
+let btnA = document.getElementById('a');
+let btnB = document.getElementById('b');
+let btnC = document.getElementById('c');
+let btnD = document.getElementById('d');
