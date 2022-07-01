ce30c025087675fcf56b81def2e664592da2fe01
Jeannette Torres <jeannette.ocegueda@yahoo.com>
2022-06-30 22:18:20 -0700

    added my first question in js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // Start of assignment
 // Variables
+let startBtn = document.getElementById('start-btn');
 let quizPage = document.getElementById('quiz-page');
 let timerEl = document.getElementById('timer');
+let quizQuestions = document.getElementById('quiz-questions')
 let btnA = document.getElementById('a');
 let btnB = document.getElementById('b');
 let btnC = document.getElementById('c');
 let btnD = document.getElementById('d');
+let resultEl = document.getElementById('timer');
+
+
+// Quiz questions
+let questions = [{
+question: 'What does HTML stand for?',
+optionA: 'Hyper Text Markup Language',
+optionB: 'Hyper Mark Language',
+optionC: 'Hypertext Markup Language',
+optionD: 'Hyperwrite Mark Language',
+answer: 'c'},
+{
+
+}
+
+
+
+]
+
